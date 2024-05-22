fcac28bb41063d1d9311021c127019671cfe1fad
suskidee <109852676+suskidee@users.noreply.github.com>
2024-05-22 17:02:35 +0100

    done

---
+++ chatapp/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import *
+
+urlpatterns = [
+    path('', CreateRoom, name='create-room'),
+    path('<str:room_name>/<str:username>/', MessageView, name='room')
+]
